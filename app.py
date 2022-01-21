7d9314cf36e6edd9f5b8caeb092ab81e0a48eb4d
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-01-21 16:10:41 -0300

    Set base template and main routes

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
-from flask import Flask
+""" Simple Flask Blog WebApp
+    Developped by: Luiz Marin """
+from flask import Flask, render_template, url_for
 
+# Main App
 app = Flask(__name__)
 
+
+""" Define the routes for the webapp
+"""
+# Route for home page
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+# Route for sign up page
+@app.route("/sign-up")
+def signup():
+    return render_template('signup.html')
+
+# Route for login page
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
