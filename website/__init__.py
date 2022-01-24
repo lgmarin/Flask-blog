52d2b2009fcfb540b22a0227e331c1e3a8c405d1
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-01-24 14:12:06 -0300

    Database initialization

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from os import path
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
+# Configure Database - SQLALchemy
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     """ Create Flask App
@@ -9,9 +14,24 @@ def create_app():
 
     app = Flask(__name__)
 
+    # Database Configuration
+    app.config['SECRET_KEY'] = "SUPERSECRET"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    # Create DB
+    create_database(app)
+
     # Load Views
     from .views import views
     app.register_blueprint(views, url_prefix="/")
 
-
     return app
+
+def create_database(app):
+    """ Create a new database in the main website folder if 
+        none was found.
+    """
+    if not path.exists("website/" + DB_NAME):
+        db.create_all(app=app)
+        print("Database created.")
